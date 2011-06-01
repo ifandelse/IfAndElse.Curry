4c27a833e60761e0c459a2d29a6a0785a8c1d673
Jim Cowart <ashbylane@gmail.com>
2011-06-01 12:22:59 -0400

    Initial Add - unit tests to follow.

---
+++ IfAndElse.Curry/ActionExtensions.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IfAndElse.Curry
+{
+    public static class ActionExtensions
+    {
+        public static Action Curry<T1>(this Action<T1> d, T1 arg1)
+        {
+            return () => d.Invoke(arg1);
+        }
+
+        public static Action<T2> Curry<T1, T2>(this Action<T1, T2> d, T1 arg1)
+        {
+            return (arg2) => d.Invoke(arg1, arg2);
+        }
+
+        public static Action<T3> Curry<T1, T2, T3>(this Action<T1, T2, T3> d, T1 arg1, T2 arg2)
+        {
+            return (arg3) => d.Invoke(arg1, arg2, arg3);
+        }
+
+        public static Action<T4> Curry<T1, T2, T3, T4>(this Action<T1, T2, T3, T4> d, T1 arg1, T2 arg2, T3 arg3)
+        {
+            return (arg4) => d.Invoke(arg1, arg2, arg3, arg4);
+        }
+
+        public static Action<T5> Curry<T1, T2, T3, T4, T5>(this Action<T1, T2, T3, T4, T5> d, T1 arg1, T2 arg2, T3 arg3, T4 arg4)
+        {
+            return (arg5) => d.Invoke(arg1, arg2, arg3, arg4, arg5);
+        }
+
+        public static Action<T6> Curry<T1, T2, T3, T4, T5, T6>(this Action<T1, T2, T3, T4, T5, T6> d, T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5)
+        {
+            return (arg6) => d.Invoke(arg1, arg2, arg3, arg4, arg5, arg6);
+        }
+
+        public static Action<T7> Curry<T1, T2, T3, T4, T5, T6, T7>(this Action<T1, T2, T3, T4, T5, T6, T7> d, T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6)
+        {
+            return (arg7) => d.Invoke(arg1, arg2, arg3, arg4, arg5, arg6, arg7);
+        }
+
+        public static Action<T8> Curry<T1, T2, T3, T4, T5, T6, T7, T8>(this Action<T1, T2, T3, T4, T5, T6, T7, T8> d, T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7)
+        {
+            return (arg8) => d.Invoke(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8);
+        }
+
+        public static Action<T9> Curry<T1, T2, T3, T4, T5, T6, T7, T8, T9>(this Action<T1, T2, T3, T4, T5, T6, T7, T8, T9> d, T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8)
+        {
+            return (arg9) => d.Invoke(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9);
+        }
+
+        public static Action<T10> Curry<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10>(this Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10> d, T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8, T9 arg9)
+        {
+            return (arg10) => d.Invoke(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10);
+        }
+
+        public static Action<T11> Curry<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11>(this Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11> d, T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8, T9 arg9, T10 arg10)
+        {
+            return (arg11) => d.Invoke(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11);
+        }
+    }
+}
---
+++ IfAndElse.Curry/FuncExtensions.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace IfAndElse.Curry
+{
+    public static class FuncExtensions
+    {
+        public static Func<TResult> Curry<T1, TResult>(this Func<T1, TResult> f, T1 arg1)
+        {
+            return () => f.Invoke(arg1);
+        }
+
+        public static Func<T2, TResult> Curry<T1, T2, TResult>(this Func<T1, T2, TResult> f, T1 arg1)
+        {
+            return (arg2) => f.Invoke( arg1, arg2 );
+        }
+
+        public static Func<T3, TResult> Curry<T1, T2, T3, TResult>(this Func<T1, T2, T3, TResult> f, T1 arg1, T2 arg2)
+        {
+            return (arg3) => f.Invoke(arg1, arg2, arg3);
+        }
+
+        public static Func<T4, TResult> Curry<T1, T2, T3, T4, TResult>(this Func<T1, T2, T3, T4, TResult> f, T1 arg1, T2 arg2, T3 arg3)
+        {
+            return (arg4) => f.Invoke(arg1, arg2, arg3, arg4);
+        }
+
+        public static Func<T5, TResult> Curry<T1, T2, T3, T4, T5, TResult>(this Func<T1, T2, T3, T4, T5, TResult> f, T1 arg1, T2 arg2, T3 arg3, T4 arg4)
+        {
+            return (arg5) => f.Invoke(arg1, arg2, arg3, arg4, arg5);
+        }
+
+        public static Func<T6, TResult> Curry<T1, T2, T3, T4, T5, T6, TResult>(this Func<T1, T2, T3, T4, T5, T6, TResult> f, T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5)
+        {
+            return (arg6) => f.Invoke(arg1, arg2, arg3, arg4, arg5, arg6);
+        }
+
+        public static Func<T7, TResult> Curry<T1, T2, T3, T4, T5, T6, T7, TResult>(this Func<T1, T2, T3, T4, T5,T6, T7, TResult> f, T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6)
+        {
+            return (arg7) => f.Invoke(arg1, arg2, arg3, arg4, arg5, arg6, arg7);
+        }
+
+        public static Func<T8, TResult> Curry<T1, T2, T3, T4, T5, T6, T7, T8, TResult>(this Func<T1, T2, T3, T4, T5, T6, T7, T8, TResult> f, T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7)
+        {
+            return (arg8) => f.Invoke(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8);
+        }
+
+        public static Func<T9, TResult> Curry<T1, T2, T3, T4, T5, T6, T7, T8, T9, TResult>(this Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, TResult> f, T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8)
+        {
+            return (arg9) => f.Invoke(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9);
+        }
+
+        public static Func<T10, TResult> Curry<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, TResult>(this Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, TResult> f, T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8, T9 arg9)
+        {
+            return (arg10) => f.Invoke(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10);
+        }
+
+        public static Func<T11, TResult> Curry<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, TResult>(this Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, TResult> f, T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8, T9 arg9, T10 arg10)
+        {
+            return (arg11) => f.Invoke(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11);
+        }
+    }
+}
